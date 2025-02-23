d08ee11fba66690be9d30380a76c90cc46274d26
Altairian7 <artisharma.as12.as@gmail.com>
2025-02-23 16:44:36 +0530

    created project and setup baisc routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 21 }}>This is all about me</h1>{" "}
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div>
+        <h1 style={{ fontSize: 21 }}>This is where you contact me</h1>{" "}
+      </div>
+    );
+  }
+  
