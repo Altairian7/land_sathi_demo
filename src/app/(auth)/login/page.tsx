971181ecd0156a24f575a2d042cde85cdb465a0c
Altairian7 <artisharma.as12.as@gmail.com>
2025-02-24 12:03:39 +0530

    added route groups for auth

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Enter your email address and password to log in</p>
+    </div>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,8 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Enter your email address and password to register</p>
+    </div>
+  );
+}
